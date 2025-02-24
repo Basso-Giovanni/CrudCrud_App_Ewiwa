0d215f2ddf76771aad393a3a8c9628e7a6042013
9698115 <9698115@itisrossi.vi.it>
2025-02-24 12:17:26 +0100

    init

---
+++ app/src/main/java/com/example/crudcrud_app_ewiwa/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.crudcrud_app_ewiwa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.crudcrud_app_ewiwa.ui.theme.CrudCrud_App_EwiwaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CrudCrud_App_EwiwaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CrudCrud_App_EwiwaTheme {
+        Greeting("Android")
+    }
+}
