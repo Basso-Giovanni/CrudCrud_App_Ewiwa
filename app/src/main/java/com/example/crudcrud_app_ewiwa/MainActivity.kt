94c09305ca7fd4175a3c1bb72b344095af1cc0e6
9698115 <9698115@itisrossi.vi.it>
2025-02-24 12:57:27 +0100

    interfaccia

--- app/src/main/java/com/example/crudcrud_app_ewiwa/MainActivity.kt
+++ app/src/main/java/com/example/crudcrud_app_ewiwa/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.crudcrud_app_ewiwa.ui.theme.CrudCrud_App_EwiwaTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +34,72 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CrudCrud_App_EwiwaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ColumnExample()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ColumnExample() {
+    var input by remember { mutableStateOf("") }
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center)
+    {
+        Box(modifier = Modifier.padding(16.dp))
+        {
+            Button(onClick = { }, modifier = Modifier.size(200.dp, 60.dp))
+            {
+                Text("GET", style = TextStyle(fontSize = 20.sp))
+            }
+        }
+        Box(modifier = Modifier.padding(16.dp))
+        {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center)
+            {
+                TextField(
+                    modifier = Modifier.padding(16.dp),
+                    value = input,
+                    label = {Text("Aggiungi")},
+                    onValueChange = {input = it}
+                )
+                Button(onClick = { }, modifier = Modifier.size(200.dp, 60.dp))
+                {
+                    Text("POST", style = TextStyle(fontSize = 20.sp))
+                }
 
+            }
+        }
+        Box(modifier = Modifier.padding(16.dp))
+        {
+            Column {
+                Button(onClick = { },
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                        .size(200.dp, 60.dp)
+                    )
+                {
+                    Text("PUT", style = TextStyle(fontSize = 20.sp))
+                }
+                Button(onClick = { },
+                    modifier = Modifier.size(200.dp, 60.dp))
+                {
+                    Text("DELETE", style = TextStyle(fontSize = 20.sp))
+                }
+
+            }
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CrudCrud_App_EwiwaTheme {
-        Greeting("Android")
+        ColumnExample()
     }
 }
